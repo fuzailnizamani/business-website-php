c8815e006914a2a01d35de48d975a7af23c0076d
fuzailnizmani <fuzailnizamani13@gmail.com>
2025-05-14 15:49:04 +0500

    updated footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
  <h2 class= "credit">Fuzail Nizamani</h2>
  <h2 class= "credit">2k22/CSE/37</h2>
  <footer>
-    <p>&copy; <?php echo date("Y"); ?> Foodie's Delight. All rights reserved.</p>
+    <p>&copy; <?php echo date("Y"); ?> Best Food. All rights reserved.</p>
   </footer>
 </body>
 </html>
